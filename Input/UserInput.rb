f3d4955de8188e123f19be7eaa7001718826cb63
Agrima Saxena <agrimalc@gmail.com>
2026-08-25 01:58:41 +0530

    update project files

--- Input/UserInput.rb
+++ Input/UserInput.rb
@@ -1,3 +1,8 @@
 puts "Enter you name : "
 name=gets()
 puts("Hey there "+name+", You are so cool")
+puts "Enter your name: "
+puts "Enter your age: "
+name=gets.chomp()
+age=gets.chomp()
+puts("Hey there "+name+", You are "+age+" years old")
--- Numbers/nums.rb
+++ Numbers/nums.rb
@@ -5,3 +5,5 @@ puts 10.0%5
 puts 2**3 #//Math.pow(2,3) is not supported in Ruby
 puts Math.sqrt(36)
 puts Math.log(1)
+num=15
+puts ("My birthday is on "+num.to_s+"th of January")
