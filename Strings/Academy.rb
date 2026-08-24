b383857401249ffea1d5b12fd0c1e4ad40fb9e67
Agrima Saxena <agrimalc@gmail.com>
2026-08-25 01:31:47 +0530

    update project files

--- Strings/Academy.rb
+++ Strings/Academy.rb
@@ -1,3 +1,7 @@
 phrase="Dance Academy"
 puts phrase.upcase()
 puts phrase.downcase()
+phrase="  Dance Academy  "
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include?("Academy")
---
+++ Strings/Giraffe.rb
@@ -0,0 +1,6 @@
+phrase="Giraffe Academy"
+puts phrase[0]
+puts phrase[0,3]
+puts phrase.index("G")
+puts phrase.index("ffe")
+puts "Academy".upcase()
